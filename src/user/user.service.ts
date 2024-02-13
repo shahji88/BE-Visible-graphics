a2888ed0116e053510182c98496105e9cef48296
susan7872 <tukilogic@gmail.com>
2024-02-13 13:37:36 +0545

    BE - Implement Auth0 for user authentication

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,12 +4,4 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
-
-  async createUser(email: string, password: string, role: string, status: string) {
-    return this.prismaService.createUser(email, password, role, status);
-  }
-
-  async findUserByEmail(email: string) {
-    return this.prismaService.findUserByEmail(email);
-  }
 }
